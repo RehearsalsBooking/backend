ab6a135003b4aad02ee666ee9d1c067038cbfa98
belamov <belamov44@gmail.com>
2020-05-05 12:39:55 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -25,7 +25,6 @@ use Illuminate\Routing\Middleware\ValidateSignature;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
-use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 class Kernel extends HttpKernel
 {
