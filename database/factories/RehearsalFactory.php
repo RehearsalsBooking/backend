7636a448b3cab03ab7ff355027f91035e477d6b0
belamov <belamov@users.noreply.github.com>
2021-01-05 16:56:15 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- database/factories/RehearsalFactory.php
+++ database/factories/RehearsalFactory.php
@@ -30,8 +30,8 @@ class RehearsalFactory extends Factory
     private function getRehearsalTime(): TimestampRange
     {
         $startsAt = Carbon::create(
-            date("Y"),
-            $this->faker->numberBetween(date("m"), 12),
+            date('Y'),
+            $this->faker->numberBetween(date('m'), 12),
             $this->faker->numberBetween(1, 20),
             $this->faker->numberBetween(8, 20),
             0,
