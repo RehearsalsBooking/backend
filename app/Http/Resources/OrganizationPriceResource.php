89eea1c7517a0c568f5717040963cf2648370cef
belamov <business@belamov.com>
2021-07-18 03:56:34 +0500

    added price id to price resource

--- app/Http/Resources/OrganizationPriceResource.php
+++ app/Http/Resources/OrganizationPriceResource.php
@@ -3,25 +3,17 @@
 namespace App\Http\Resources;
 
 use App\Models\Organization\OrganizationPrice;
-use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 /**
- * Class OrganizationPriceResource.
- *
  * @mixin OrganizationPrice
  */
 class OrganizationPriceResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param Request $request
-     * @return array
-     */
     public function toArray($request): array
     {
         return [
+            'id' => $this->id,
             'day' => $this->day,
             'price' => $this->price,
             'starts_at' => $this->time->from(),
