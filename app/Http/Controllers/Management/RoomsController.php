ae9c988bebf368cf96dc82a4ac3e718fd75843a6
belamov <business@belamov.com>
2021-11-14 20:59:22 +0500

    added enpoint for updating room

--- app/Http/Controllers/Management/RoomsController.php
+++ app/Http/Controllers/Management/RoomsController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Management\CreateRoomRequest;
 use App\Http\Resources\RoomResource;
 use App\Models\Organization\Organization;
+use App\Models\Organization\OrganizationRoom;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
@@ -22,6 +23,18 @@ class RoomsController extends Controller
         $room = $organization->rooms()->create($request->getAttributes());
 
         return (new RoomResource($room))->response()->setStatusCode(Response::HTTP_CREATED);
+    }
+
+    /**
+     * @throws AuthorizationException
+     */
+    public function update(CreateRoomRequest $request, Organization $organization, OrganizationRoom $room): RoomResource
+    {
+        $this->authorize('manage', $organization);
+
+        $room->update($request->getAttributes());
+
+        return new RoomResource($room);
     }
 
 }
