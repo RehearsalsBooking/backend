6d961614882d7a4e82fda6786b767d8b2b4ca3da
belamov <business@belamov.com>
2022-05-12 16:13:49 +0500

    debug hosts

--- app/Http/Middleware/TrustHosts.php
+++ app/Http/Middleware/TrustHosts.php
@@ -3,14 +3,23 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Middleware\TrustHosts as Middleware;
+use Illuminate\Http\Request;
 
 class TrustHosts extends Middleware
 {
     public function hosts(): array
     {
+        try {
+            \Log::info(Request::capture()->getHost());
+
+        } catch (\Throwable $exception){
+            \Log::error($exception);
+        }
         return [
             'app.festic.ru',
             'demo.festic.ru',
+            'https://app.festic.ru',
+            'https://demo.festic.ru',
             'rehearsals.local',
             'localhost'
         ];
